abb7964d5edfd516c1e0536bdf8e78b7b62d1a4c
ghongli <18810027593@163.com>
2021-12-12 23:13:38 +0800

    issues: context done test

--- issues/context_test.go
+++ issues/context_test.go
@@ -14,19 +14,25 @@ func TestTimeoutDone(t *testing.T) {
 	t.Run("timeout duration gt parent ctx", func(t *testing.T) {
 		go ctxTimeoutDone(ctx, 1500 * time.Millisecond)
 		
-		select {
-		case <-ctx.Done():
+		if <-ctx.Done(); true {
 			fmt.Println("main", ctx.Err())
 		}
+		
+		// output:
+		// handler context deadline exceeded
+		// main context deadline exceeded
 	})
 	
 	t.Run("timeout duration lt parent ctx", func(t *testing.T) {
 		go ctxTimeoutDone(ctx, 500 * time.Millisecond)
 		
-		select {
-		case <-ctx.Done():
+		if <-ctx.Done(); true {
 			fmt.Println("main", ctx.Err())
 		}
+		
+		// output:
+		// process request with 500ms
+		// main context deadline exceeded
 	})
 	
 }
