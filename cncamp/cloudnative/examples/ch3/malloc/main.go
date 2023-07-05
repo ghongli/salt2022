60b47e3a302e7e12a520d479436d7242010f862c
ghongli <18810027593@163.com>
2023-07-05 18:53:53 +0800

    cncamp: cloudnative ch3 malloc

--- cncamp/cloudnative/examples/ch3/malloc/main.go
+++ cncamp/cloudnative/examples/ch3/malloc/main.go
@@ -1,5 +1,16 @@
 package main
 
+/**
+CGO_ENABLED=1，进行编译时，会将文件中引用libc的库(如常用的net包)，以动态链接的方式生成目标文件 -- 默认；
+CGO_ENABLED=0，进行编译时，则会把在目标文件中未定义的符号(外部函数)一起链接到可执行文件中；
+ldd target_file 查看引用的动态链接库
+nm  target_file 查看符号表
+nm main0 | grep ' U '
+
+ldd: print shared library depencies
+nm:  list symbols from object files
+*/
+
 //#cgo LDFLAGS:
 //char* allocMemory();
 import "C"
